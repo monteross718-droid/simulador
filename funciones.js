cd171672fd7dbdd9b890ccf796ba7493e5caa59e
monteross718-droid <monteross718@gmail.com>
2026-09-24 20:04:26 -0500

    parte 2 y 3 con casos de prueba

--- funciones.js
+++ funciones.js
@@ -1,1 +1,16 @@
 //AQUI TODA LA LOGICA DE LAS FUNCIONES DEL NEGOCIO
+
+let ingresos;
+let egresos;
+let valorDisponible;
+
+function calcularDisponible(ingresos, egresos){
+    valorDisponible = Math.round((ingresos - egresos)*100) / 100 ;
+
+    if(valorDisponible < 0){
+    return 0;
+
+    }
+        return valorDisponible;
+}
+
--- simulador.js
+++ simulador.js
@@ -1,1 +1,11 @@
 //AQUI EL JAVASCRIPT PARA MANIPULAR EL HTML
+
+function calcular(){
+ 
+let  ingresos = parseFloat(document.getElementById("txtIngresos").value);
+let  egresos = parseFloat(document.getElementById("txtEgresos").value);
+
+let valorDisponible = calcularDisponible(ingresos, egresos);
+document.getElementById("lblDisponibleValor").innerText = valorDisponible;
+
+}
